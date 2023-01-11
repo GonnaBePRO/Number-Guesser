509d6cf99a3bf0985243ae0d53aeec2428655a17
Michu <michu9139@gmail.com>
2023-01-11 21:34:36 +0100

    Lose Case & Game Over
    Lose Case & Game Over

--- app.js
+++ app.js
@@ -27,16 +27,50 @@ guessBtn.addEventListener('click', function(){
 
     // Check if won
     if(guess === winningNum) {
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct, YOU WIN`, 'green');
-    } else {
+        // Game Over - won
 
+        gameOver(true, `${winningNum} is correct, YOU WIN`); 
+
+    } else {
+        // Wrong number
+        guessesLeft -= 1;
+
+        if(guessesLeft === 0){
+            //Game Over - lost
+
+           gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+
+        }else {
+            //Game continues - answer wrong 
+
+            // Change border color
+            guessInput.style.borderColor = 'red';
+
+            //Clear the input
+            guessInput.value = '';
+
+            // Tell user its the wrong number
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red' );
+            
+        }
     }
 });
+
+// Game Over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // Set message
+    setMessage(msg);
+}
+
 
 // Set message
 function setMessage(msg, color){
